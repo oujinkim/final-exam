207288d1d0c9f042b4cec308c88341a7c2cf33b5
oujinkim <101345032+oujinkim@users.noreply.github.com>
2022-06-04 02:12:51 +0900

    Delete geneticalgorithm directory

--- geneticalgorithm/Problem.java
+++
@@ -1,5 +0,0 @@
-package geneticalgorithm;
-
-public interface Problem {
-    double fit(double x);//return -x*x + 38*x+ 80
-}
--- geneticalgorithm/main.java
+++
@@ -1,9 +0,0 @@
-package geneticalgorithm;
-
-public class main {
-    public static void main(String[] args) {
-        geneticalgorithm ga = new geneticalgorithm();
-        double solution = ga.solve(4, x-> - x*x+38*x+80);
-        System.out.println(solution);
-    }
-}
